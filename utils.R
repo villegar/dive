724789c2d960f9355a34975255f6a9133fcf24c7
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-08-04 20:15:34 -0500

    Adding new function get_md5, to recursively compute MD5 hashes

--- utils.R
+++ utils.R
@@ -12,11 +12,37 @@
 raw_md5 <- function(extension = "md5.txt", subdirectory = "") {
   files <- list.files(here::here(subdirectory), pattern = paste0("*", extension))
   raw_sequences_md5 <- data.frame() # Create empty data frame
-  for(f in files){ # Read each file containing the MD5SUM and RAW SEQUENCE NAMES
-    tmp <- read.table(f, col.names = c("MD5SUM","RAW_SEQUENCE_NAME"), stringsAsFactors = FALSE)
-    raw_sequences_md5 <- rbind(raw_sequences_md5,tmp) # Cat by rows
+  for(f in files) { # Read each file containing the MD5SUM and RAW SEQUENCE NAMES
+    tmp <- read.table(f, col.names = c("MD5SUM", "RAW_SEQUENCE_NAME"), stringsAsFactors = FALSE)
+    raw_sequences_md5 <- rbind(raw_sequences_md5, tmp) # Cat by rows
   }
   
   # Export as CSV the combined data
   write.csv(raw_sequences_md5, here::here("raw_sequences_md5.csv"))
+}
+
+get_md5 <- function(extension = "fasta", subdirectory = "") {
+  # List subdirectories inside the current working directory
+  dirs <- unique(c("", list.dirs(here::here(subdirectory),
+                                 full.names = FALSE, recursive = FALSE)))
+  # Loop through the subdirectories
+  for(d in dirs) {
+    # List files inside the current subdirectory, that match a extension
+    files <- list.files(here::here(d), pattern = paste0(".", extension, "$"))
+    md5 <- data.frame()
+    for(f in files) {
+      fullpath <- here::here(d, f) # Full path to current element
+      if (file.exists(fullpath) && !dir.exists(fullpath)) {
+        # Compute MD5 hash
+        tmp <- data.frame(MD5 = tools::md5sum(fullpath)[[1]], FILENAME = f)
+        md5 <- rbind(md5, tmp)
+      }
+    }
+    if (nrow(md5) > 0) {
+      # Store the results as a table file: MD5 FILENAME
+      output_name <- here::here(paste0(ifelse(d == "", "top", d), ".md5.txt"))
+      colnames(md5) <- NULL
+      write.table(md5, output_name, row.names = FALSE, quote = FALSE)
+    }
+  }
 }
