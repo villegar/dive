74a5b95b4313d147e027f7fa04a617cd695e0326
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-11 21:05:31 +0100

    New version of hex_logo

--- R/utils.R
+++ R/utils.R
@@ -53,17 +53,41 @@ get_md5 <- function(extension = "fasta", subdirectory = "") {
   }
 }
 
-#' Generates DIVe hexagon logo for the repository
+#' Create hexagonal logo for the package
 #'
+#' @param subplot image to use as the main logo
+#' @param dpi plot resolution (dots-per-inch)
+#' @param h_color color for hexagon border
+#' @param h_fill color to fill hexagon
+#' @param output output file (hexagonal logo)
+#' @param package title for logo (package name)
+#' @param p_color color for package name
+#' @param url URL for package repository or website
+#' @param u_size text size for URL
+#'
+#' @return hexagonal logo
 #' @export
 #'
 #' @examples
 #' hex_logo()
-hex_logo <- function() {
-  hexSticker::sticker("images/diving.png", package="DIVe", 
-                      h_color = "black",  h_fill = "white",
-                      l_x = 1, l_y = 1, spotlight = FALSE, 
-                      s_x = 1.15, s_y = 1.0, s_width = .6, 
-                      p_x = .75, p_y = 1.5, p_size = 8, p_color = "black",
-                      filename="images/logo.png")
+#' \dontrun{
+#' hex_logo("inst/images/diving.png", output = "inst/images/logo.png")
+#' }
+hex_logo <- function(subplot = system.file("images/diving.png", package = "dive"),
+                     dpi = 600,
+                     h_color = "#000000",
+                     h_fill = "#FFFFFF",
+                     output = system.file("images/logo.png", package = "dive"),
+                     package = "DIVe",
+                     p_color = "#000000",
+                     url = "https://github.com/villegar/dive",
+                     u_size = 1.55) {
+  hexSticker::sticker(subplot = subplot, package = package,
+                      h_color = h_color,  h_fill = h_fill,
+                      dpi = dpi,
+                      s_x = 1.0, s_y = .85, s_width = .5,
+                      p_x = 1.0, p_y = 1.52, p_size = 6, p_color = p_color,
+                      url = url,
+                      u_angle = 30, u_color = p_color, u_size = u_size,
+                      filename = output)
 }
